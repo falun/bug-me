b54cbea43bcf822d8b23ac3ea033c4b9ef7a740c
falun <richard@turbinelabs.io>
2016-03-23 09:12:59 -0700

    update tests to work with new infar & rename family to matcher.go

--- itemstore/matcher_test.go
+++ itemstore/matcher_test.go
@@ -4,56 +4,62 @@ import (
 	"testing"
 )
 
-func testTrue(i Item) bool  { return true }
-func testFalse(i Item) bool { return false }
+type TestTrue struct{}
+type TestFalse struct{}
+
+func (_ TestTrue) Test(_ Item) bool  { return true }
+func (_ TestFalse) Test(_ Item) bool { return false }
+
+var True Matcher = Leaf{TestTrue{}}
+var False Matcher = Leaf{TestFalse{}}
 
 func TestItemFilterSimpleAnd(t *testing.T) {
-	f := NewFilter(testTrue)
-	f = f.And(NewFilter(testTrue))
+	f := True
+	f = f.And(True)
 	if !f.Match(nil) {
 		t.Errorf("true && true must be true")
 	}
 
-	f = NewFilter(testFalse)
-	f = f.And(NewFilter(testTrue))
+	f = False
+	f = f.And(True)
 	if f.Match(nil) {
 		t.Errorf("false && true must be false")
 	}
 
-	f = NewFilter(testTrue)
-	f = f.And(NewFilter(testFalse))
+	f = True
+	f = f.And(False)
 	if f.Match(nil) {
 		t.Errorf("true && false must be false")
 	}
 
-	f = NewFilter(testFalse)
-	f = f.And(NewFilter(testFalse))
+	f = False
+	f = f.And(False)
 	if f.Match(nil) {
 		t.Errorf("false && false must be false")
 	}
 }
 
 func TestItemFilterSimpleOr(t *testing.T) {
-	f := NewFilter(testTrue)
-	f = f.Or(NewFilter(testTrue))
+	f := True
+	f = f.Or(True)
 	if !f.Match(nil) {
 		t.Errorf("true || true must be true")
 	}
 
-	f = NewFilter(testFalse)
-	f = f.Or(NewFilter(testTrue))
+	f = False
+	f = f.Or(True)
 	if !f.Match(nil) {
 		t.Errorf("false || true must be true")
 	}
 
-	f = NewFilter(testTrue)
-	f = f.Or(NewFilter(testFalse))
+	f = True
+	f = f.Or(False)
 	if !f.Match(nil) {
 		t.Errorf("true || false must be true")
 	}
 
-	f = NewFilter(testFalse)
-	f = f.Or(NewFilter(testFalse))
+	f = False
+	f = f.Or(False)
 	if f.Match(nil) {
 		t.Errorf("false || false must be false")
 	}
@@ -61,9 +67,9 @@ func TestItemFilterSimpleOr(t *testing.T) {
 
 func TestItemFilterGroupRight(t *testing.T) {
 	// true && (false || true)
-	f := NewFilter(testTrue)
-	f2 := NewFilter(testFalse)
-	f2 = f2.Or(NewFilter(testTrue))
+	f := True
+	f2 := False
+	f2 = f2.Or(True)
 	f = f.And(f2)
 	if !f.Match(nil) {
 		t.Errorf("true && (false || true) must be true")
@@ -72,9 +78,9 @@ func TestItemFilterGroupRight(t *testing.T) {
 
 func TestItemFilterGroupLeft(t *testing.T) {
 	// (false || true) && true
-	f := NewFilter(testFalse)
-	f = f.Or(NewFilter(testTrue))
-	f2 := NewFilter(testTrue)
+	f := False
+	f = f.Or(True)
+	f2 := True
 	f = f.And(f2)
 	if !f.Match(nil) {
 		t.Errorf("(false || true) && true must be true")
